d563cae72e656b23887cfba0b352aed27f2a4b28
saurabhraut1212 <saurabhrmahadev1212@gmail.com>
2024-10-23 13:55:54 +0530

    fix: healthnfinity login

--- src/pages-section/login/index.js
+++ src/pages-section/login/index.js
@@ -66,7 +66,7 @@ const LoginForm = () => {
                     {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>}
                 </div>
 
-                =
+
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm">Password</label>
                     <input
